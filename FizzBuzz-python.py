5fd944583a93d297accb7a250c9994c6b1cdf662
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-23 19:35:32 -0300

    Update FizzBuzz-python.py

--- FizzBuzz-python.py
+++ FizzBuzz-python.py
@@ -1,5 +1,4 @@
 #
-#
 # Autor:
 # Michel Silva
 # data: 23/07/2022
@@ -13,19 +12,19 @@
 # Se i não for múltiplo de 3 ou 5, imprima o valor de i.
 
 # função principal
-def fizzbuzz(n):
-    for i in range(1, n+1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+def fizzbuzz(n):  # n é o número de vezes que o programa deve rodar o loop de 1 a n
+    for i in range(1, n+1): # loop de 1 a n
+        if i % 3 == 0 and i % 5 == 0: # se i for múltiplo de 3 e 5
+            print("FizzBuzz") # imprime FizzBuzz
+        elif i % 3 == 0: # se i for múltiplo de 3
+            print("Fizz") # imprime Fizz
+        elif i % 5 == 0: # se i for múltiplo de 5
+            print("Buzz") # imprime Buzz
+        else: # se não for múltiplo de 3 ou 5
+            print(i) # imprime o valor de i
 
 
 # chamada da função principal
-valor = int(input("Digite um valor: "))
-fizzbuzz(valor)
-
+valor = int(input("Digite um valor: ")) # valor é o número de vezes que o programa deve rodar o loop de 1 a valor
+fizzbuzz(valor) # chamada da função fizzbuzz com o valor de valor
+print("Fim do programa")
